8faf506d7ea7923cc7ade9232a8528c0ab033969
Marketing Director <nicolestark1103@gmail.com>
2023-08-22 10:13:19 -0500

    starting over, too messy

--- server.js
+++ server.js
@@ -5,35 +5,6 @@ const connection = require('./db/connection')
 connection.connect(err => {
     if (err) throw (err)
     console.log('Connection successful!')
-    employeeTracker()
+    userQuestions()
 })
 
-const employeeTracker = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'action',
-            message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Departmnet']
-        }
-    ])
-    .then(answers => {
-        // View All Employees
-        if (answers.prompt === 'View All Employees') {
-            connection.query(`SELECT * FROM employee`, (err, result) => {
-                if (err) throw err
-                console.table(result)
-                employeeTracker()
-            })
-            
-        // View All Roles 
-        } else if (answers.prompt === 'View All Roles') {
-            connection.query(`SELECT * FROM role`, (err, result) => {
-                if (err) throw err
-                console.table(result)
-                employeeTracker
-            })
-
-        }
-    })
-}
