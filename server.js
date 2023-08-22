12d0458e218c4bb66e3cc5d6574e2e2cca90970f
Marketing Director <nicolestark1103@gmail.com>
2023-08-22 12:56:17 -0500

    view employee by manager working, js done, readme next

--- server.js
+++ server.js
@@ -408,6 +408,19 @@ viewEmployeesByDepartment = () => {
                       })
 }
 
+// function to view employee by manager
+viewEmployeesByManager = () => {
+    connection.query(`SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title FROM employee
+                      LEFT JOIN employee manager ON manager.id = employee.manager_id
+                      INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+                      INNER JOIN department ON (department.id = role.department_id)
+                      ORDER BY manager`, (err, result) => {
+                        if (err) throw err
+                        console.table(result)
+                        questions()
+                      })
+}
+
 // function to delete employee
 deleteEmployee = () => {
     connection.query(`SELECT * FROM employee`, (err, result) => {
